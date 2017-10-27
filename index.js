34089a09b00b271d55d30b4fa81f6a62ef7467a7
tom-aglow <tom@ohhhh.me>
2017-10-27 08:48:02 -0700

    simulate login and logout with cookie-session module and passport

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
@@ -7,6 +10,14 @@ require('./services/passport');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 //	auth routes
 require('./routes/authRoutes')(app);
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -11,4 +11,13 @@ module.exports = app => {
 
   //	route handler for Google OAUTH callback
   app.get('/auth/google/callback', passport.authenticate('google'));
+
+  app.get('/api/logout', (req, res) => {
+    req.logout();
+    res.send(req.user);
+  });
+
+  app.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+  });
 };
