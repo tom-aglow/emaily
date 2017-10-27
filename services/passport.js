d207fe617e539b06711f46332ec0079bde4ca30e
tom-aglow <tom@ohhhh.me>
2017-10-26 21:58:58 -0700

    finish GoogleStrategy callback

--- services/passport.js
+++ services/passport.js
@@ -1,16 +1,28 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
+const User = mongoose.model('users');
+
 passport.use(
-	new GoogleStrategy(
-		{
-			clientID: keys.googleClientID,
-			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
-		},
-		(accessToken, refreshToken, profile, done) => {
-			console.log(accessToken);
-		}
-	)
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    //	handle the callback from Google OAUTH
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleID: profile.id }).then(existingUser => {
+        if (existingUser) {
+          done(null, existingUser);
+        } else {
+          new User({ googleID: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
 );
