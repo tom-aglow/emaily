f2975227ff2274f5e1ef26e7a98b3b19315a2648
tom-aglow <tom@ohhhh.me>
2017-10-26 20:27:16 -0700

    add google OAUTH using passport library

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+	googleClientID: '106166519793-ofan7jbtspjckmclr2s1gsaf0s6vmd3n.apps.googleusercontent.com',
+	googleClientSecret: 'Tma5wj_sUAsOvfkYsBV4yUSl'
+};
--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken, refreshToken, profile, done) => {
+  	console.log(accessToken);
+	})
+);
+
+//	route handler to kick user to google authentication
+app.get('/auth/google', passport.authenticate('google', {
+	scope: ['profile', 'email']
+}));
+
+//	route handler for Google OAUTH callback
+app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
