3e10814866262408a3da457e20e4b7e250b852ee
tom-aglow <tom@ohhhh.me>
2017-10-28 14:18:24 -0700

    add redirect for login and logout routes handler

--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -8,7 +8,6 @@ export default function(state = null, action) {
   switch (action.type) {
     case FETCH_USER:
       return action.payload || false;
-      break;
     default:
       return state;
   }
