cface93c8f5d402695eab32ef429e095e4326298
tom-aglow <tom@ohhhh.me>
2017-10-28 14:00:04 -0700

    display different header for logged in and not logged in users using data from central state store

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,24 +1,56 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class Header extends Component {
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        return null;
+      case false:
+        return (
+          <a className="navbar-item" href="/auth/google">
+            Login with Google
+          </a>
+        );
+      default:
+        return (
+          <a className="navbar-item" href="/api/logout">
+            Logout
+          </a>
+        );
+    }
+  }
+
   render() {
     return (
-			<nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-				<div className="container">
-					<div className="navbar-brand">
-						<a className="navbar-item" href="https://bulma.io">
-							<img src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png" alt="Bulma: a modern CSS framework based on Flexbox" />
-						</a>
-					</div>
-					<div className="navbar-menu">
-						<div className="navbar-end">
-							<a className="navbar-item">Login with Google</a>
-						</div>
-					</div>
-				</div>
-			</nav>
-		);
+      <nav
+        className="navbar is-dark"
+        role="navigation"
+        aria-label="main navigation"
+      >
+        <div className="container">
+          <div className="navbar-brand">
+            <a className="navbar-item" href="/">
+              <img
+                src="https://upload.wikimedia.org/wikipedia/commons/a/ab/Android_O_Preview_Logo.png"
+                alt="Bulma: a modern CSS framework based on Flexbox"
+              />
+            </a>
+          </div>
+          <div className="navbar-menu">
+            <div className="navbar-end">
+              {this.renderContent()}
+              {/*<a className="navbar-item">Login with Google</a>*/}
+            </div>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header;
+function mapStateToProps({ auth }) {
+  return { auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,5 +1,14 @@
-export default function(state = {}, action) {
+import { FETCH_USER } from '../actions/types';
+
+export default function(state = null, action) {
+  //	null = we don't know weather user is logged in
+  //	User model = user is logged in
+  //	false = user isn't logged in
+
   switch (action.type) {
+    case FETCH_USER:
+      return action.payload || false;
+      break;
     default:
       return state;
   }
