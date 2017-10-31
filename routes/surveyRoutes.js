b56c03e6f4bc9cf629d13d0a93081042e8df74c9
tom-aglow <tom@ohhhh.me>
2017-10-30 22:06:06 -0700

    update appropriate survey in db after we get feedback from sendgrid

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -10,7 +10,7 @@ const surveyTemplate = require('../services/email_templates/surveyTemplates');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thank for voting');
   });
 
@@ -42,18 +42,32 @@ module.exports = app => {
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
 
-    const events = _.chain(req.body)
-      .map(req.body, ({ email, url }) => {
-        const match = p.test(new URL(url).pathname);
-        if (match) {
-          return { email, ...match };
-        }
-      })
-      .compact()
-      .unionBy('email', 'serveyId')
+		_.chain(req.body)
+			.map(({ email, url }) => {
+				const match = p.test(new URL(url).pathname);
+				if (match) {
+					return { email, surveyId: match.surveyId, choice: match.choice };
+				}
+			})
+			.compact()
+			.uniqBy('email', 'surveyId')
+			.each(({ surveyId, email, choice }) => {
+				Survey.updateOne(
+					{
+						_id: surveyId,
+						recipients: {
+							$elemMatch: { email: email, responded: false }
+						}
+					},
+					{
+						$inc: { [choice]: 1 },
+						$set: { 'recipients.$.responded': true },
+						lastResponded: new Date()
+					}
+				).exec();
+			})
 			.value();
 
-    console.log(events);
     res.send({});
   });
 };
